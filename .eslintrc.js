aa50eba4e16ab521504c0d22c3253f467abcef1d
paiva2 <joaovitor.paiva145@hotmail.com>
2024-03-10 00:35:54 -0300

    chore: add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: ['airbnb-base', 'airbnb-typescript/base'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  ignorePatterns: [
+    'node_modules/',
+    '**/node_modules/',
+    '/**/node_modules/*',
+    'out/',
+    'dist/',
+    'build/',
+  ],
+  rules: {
+    'class-methods-use-this': 'off',
+    'no-restricted-syntax': 'off',
+    "max-classes-per-file": "off",
+    'import/export': 'off',
+    'import/prefer-default-export': 'off',
+    'no-promise-executor-return': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'import/no-extraneous-dependencies': 'off'
+  },
+};
