a023830ceb4530ef3b2cf07c90b99a0e067e8bc1
paiva2 <joaovitor.paiva145@hotmail.com>
2024-03-13 18:25:45 -0300

    feat: UpdateUserService methods and tests

--- src/http/domain/services/user/update-user-service/update-user-service.spec.ts
+++ src/http/domain/services/user/update-user-service/update-user-service.spec.ts
@@ -4,6 +4,7 @@ import {
 import { UpdateUserService } from '..';
 import { UserRepository } from '../../../../data/repositories';
 import { UserEntity, CreateUserEntity, UpdateUserEntity } from '../../../entities';
+import { AlreadyExistsException, InvalidParamException, NotFoundException } from '../../../../presentation/exceptions';
 import { Encrypter } from '../../../protocols';
 
 const makeEncrypterStub = () => {
@@ -40,7 +41,7 @@ const makeUserRepositoryStub = () => {
 
   class UserRepositoryStub implements UserRepository {
     async findByEmail(email: string): Promise<UserEntity | null> {
-      return {} as UserEntity;
+      return null;
     }
 
     async save(user: CreateUserEntity): Promise<UserEntity> {
@@ -103,5 +104,241 @@ describe('Update user profile controller', () => {
     await sut.exec(requestBody);
 
     expect(spySut).toHaveBeenCalledWith(requestBody);
+  });
+
+  test('Should throw an error if provided zipcode is not valid', async () => {
+    const { sut } = sutFactory;
+
+    vi.spyOn(sut, 'postalCodeCheck').mockReturnValueOnce(false);
+
+    const requestBody = {
+      userId: 'valid_id',
+      zipcode: 'invalid_zipcode',
+    };
+
+    const expectedException = new InvalidParamException('zipcode');
+
+    await expect(sut.exec(requestBody)).rejects.toStrictEqual(expectedException);
+  });
+
+  test('Should throw an error if provided contact is not valid', async () => {
+    const { sut } = sutFactory;
+
+    vi.spyOn(sut, 'contactCheck').mockReturnValueOnce(false);
+
+    const requestBody = {
+      userId: 'valid_id',
+      contact: 'invalid_contact',
+    };
+
+    const expectedException = new InvalidParamException('contact');
+
+    await expect(sut.exec(requestBody)).rejects.toStrictEqual(expectedException);
+  });
+
+  test('Should throw an error if provided email is not valid', async () => {
+    const { sut } = sutFactory;
+
+    vi.spyOn(sut, 'emailCheck').mockReturnValueOnce(false);
+
+    const requestBody = {
+      userId: 'valid_id',
+      email: 'invalid_email',
+    };
+
+    const expectedException = new InvalidParamException('email');
+
+    await expect(sut.exec(requestBody)).rejects.toStrictEqual(expectedException);
+  });
+
+  test('Should throw an error if provided password has less than 6 characters', async () => {
+    const { sut } = sutFactory;
+
+    const requestBody = {
+      userId: 'valid_id',
+      password: '12345',
+    };
+
+    const expectedException = new InvalidParamException('password');
+
+    await expect(sut.exec(requestBody)).rejects.toStrictEqual(expectedException);
+  });
+
+  test('Should throw an error if provided userId doesnt exists', async () => {
+    const { sut, userRepositoryStub } = sutFactory;
+
+    vi.spyOn(userRepositoryStub, 'findById').mockReturnValueOnce(Promise.resolve(null));
+
+    const requestBody = {
+      userId: 'valid_id',
+    };
+
+    const expectedException = new NotFoundException('User');
+
+    await expect(sut.exec(requestBody)).rejects.toStrictEqual(expectedException);
+  });
+
+  test('Should throw an error if provided DTO has an email and email already exists', async () => {
+    const { sut, userRepositoryStub } = sutFactory;
+
+    const existentUser = {
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'existent_email@email.com',
+      contact: 'valid_contact',
+      password: 'valid_hashed_password',
+      neighbourhood: 'valid_neighbourhood',
+      street: 'valid_street',
+      city: 'valid_city',
+      state: 'valid_state',
+      number: 'valid_number',
+      complement: 'valid_complement',
+      zipcode: 'valid_zipcode',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    vi.spyOn(userRepositoryStub, 'findByEmail').mockReturnValueOnce(Promise.resolve(existentUser));
+
+    const requestBody = {
+      userId: 'valid_id',
+      email: 'existent_email@email.com',
+    };
+
+    const expectedException = new AlreadyExistsException('E-mail');
+
+    await expect(sut.exec(requestBody)).rejects.toStrictEqual(expectedException);
+  });
+
+  test('Should call Encrypter hash method with correct provided params if new password is provided', async () => {
+    const { sut, encrypterStub } = sutFactory;
+
+    const spyEncrypterStub = vi.spyOn(encrypterStub, 'hash');
+
+    const requestBody = {
+      userId: 'valid_id',
+      password: 'new_password',
+    };
+
+    await sut.exec(requestBody);
+
+    expect(spyEncrypterStub).toHaveBeenCalledWith(requestBody.password);
+  });
+
+  test('Should call UserRepository findById method with correct provided params', async () => {
+    const { sut, userRepositoryStub } = sutFactory;
+
+    const spyUserRepositoryStub = vi.spyOn(userRepositoryStub, 'findById');
+
+    const requestBody = {
+      userId: 'valid_id',
+      password: 'new_password',
+    };
+
+    await sut.exec(requestBody);
+
+    expect(spyUserRepositoryStub).toHaveBeenCalledWith(requestBody.userId);
+  });
+
+  test('Should call UserRepository findByEmail method with correct provided params if new email is provided', async () => {
+    const { sut, userRepositoryStub } = sutFactory;
+
+    const spyUserRepositoryStub = vi.spyOn(userRepositoryStub, 'findByEmail');
+
+    const requestBody = {
+      userId: 'valid_id',
+      email: 'new_email',
+    };
+
+    await sut.exec(requestBody);
+
+    expect(spyUserRepositoryStub).toHaveBeenCalledWith(requestBody.email);
+  });
+
+  test('Should hash a new password if password is provided and pass it to UserRepository update method', async () => {
+    const { sut, userRepositoryStub } = sutFactory;
+
+    const spyUserRepositoryStub = vi.spyOn(userRepositoryStub, 'update');
+
+    const requestBody = {
+      userId: 'valid_id',
+      password: 'new_password',
+    };
+
+    await sut.exec(requestBody);
+
+    expect(spyUserRepositoryStub).toHaveBeenCalledWith({
+      ...requestBody,
+      password: 'valid_hashed_password',
+    });
+  });
+
+  test('Should return ClientUserEntity with updated infos if provided email was already mine', async () => {
+    const { sut, userRepositoryStub } = sutFactory;
+
+    const updatedClientUserEntity = {
+      id: 'valid_id',
+      name: 'new_name',
+      email: 'new_email@email.com',
+      contact: 'valid_contact',
+      password: 'valid_hashed_password',
+      neighbourhood: 'valid_neighbourhood',
+      street: 'valid_street',
+      city: 'valid_city',
+      state: 'valid_state',
+      number: 'valid_number',
+      complement: 'valid_complement',
+      zipcode: 'valid_zipcode',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    const requestBody = {
+      userId: 'valid_id',
+      email: 'new_email@email.com',
+      name: 'new_name',
+    };
+
+    vi.spyOn(userRepositoryStub, 'findByEmail').mockReturnValueOnce(Promise.resolve(updatedClientUserEntity));
+    vi.spyOn(userRepositoryStub, 'findById').mockReturnValueOnce(Promise.resolve(updatedClientUserEntity));
+
+    vi.spyOn(userRepositoryStub, 'update').mockReturnValueOnce(Promise.resolve(updatedClientUserEntity));
+
+    const response = await sut.exec(requestBody);
+
+    expect(response).toEqual(updatedClientUserEntity);
+  });
+
+  test('Should return ClientUserEntity with updated infos if nothing goes wrong', async () => {
+    const { sut, userRepositoryStub } = sutFactory;
+
+    const updatedClientUserEntity = {
+      id: 'valid_id',
+      name: 'new_name',
+      email: 'new_email@email.com',
+      contact: 'valid_contact',
+      password: 'valid_hashed_password',
+      neighbourhood: 'valid_neighbourhood',
+      street: 'valid_street',
+      city: 'valid_city',
+      state: 'valid_state',
+      number: 'valid_number',
+      complement: 'valid_complement',
+      zipcode: 'valid_zipcode',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+
+    const requestBody = {
+      userId: 'valid_id',
+      email: 'new_email@email.com',
+      name: 'new_name',
+    };
+
+    vi.spyOn(userRepositoryStub, 'update').mockReturnValueOnce(Promise.resolve(updatedClientUserEntity));
+
+    const response = await sut.exec(requestBody);
+
+    expect(response).toEqual(updatedClientUserEntity);
   });
 });
