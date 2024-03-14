d42a040c0b3b26209a61ab7eaa994f77a2f349fc
paiva2 <joaovitor.paiva145@hotmail.com>
2024-03-13 23:52:21 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { Express } from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import { globalErrorHandler } from './http/presentation/middlewares';
 import { routesHandler } from './http/presentation/routes';
 import 'express-async-errors';
 import 'dotenv/config';
 
 const app: Express = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 routesHandler(app);
